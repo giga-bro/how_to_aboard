5b7232ef08d5b15f0e6ea107b462240d5c675c91
giga-bro <aditya.sakpal2008@gmail.com>
2024-05-22 03:37:55 +0530

    Update layout.tsx

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -1,44 +1,53 @@
 // @ts-nocheck
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LoginIcon from '@mui/icons-material/Login';
 import PersonIcon from '@mui/icons-material/Person';
 import Link from 'next/link';
+import './index.css'
 
 interface LayoutProps {
     children: React.ReactNode
 }
 
 const layout: React.FC<LayoutProps> = (props) => {
+    const [active, setActive] = useState<string>('');
+
+    const handleSetActive = (path:string) => {
+        setActive(path);
+    };
+
     return (
         <>
             <div className='w-screen h-screen ' >
-                <div className='w-[100%] h-[10%]   flex justify-center items-center '>
-                    <Link href="/pages/home" className='w-[25%] h-[100%]  flex justify-center items-center' >
-                        <div className='w-[100%] h-[100%] flex  justify-center items-center   text-center text-black lg:text-2xl cursor-pointer  flex-col  text-[0.60rem]' >
-                            <HomeIcon className='w-[60%]  h-[50%] m-0 p-0 mt-[2%] text-black ' />
-                            <p> Home</p>
+                <div className='navbar'>
+                    <Link href="/pages/home" className='m-[0px]' onClick={() => handleSetActive('/pages/home')}>
+                        <div className={`nav-item ${active === '/pages/home' ? 'active' : ''}`}>
+                            <HomeIcon className="nav-icon" />
+                            <p>Home</p>
                         </div>
                     </Link>
-                    <Link href="/pages/searchcourse" className='w-[25%] h-[100%]  flex justify-center items-center' >
-                        <div className='w-[100%] h-[100%] flex justify-center items-center   text-center text-black lg:text-2xl cursor-pointer flex-col  text-[0.60rem]' >
-                            <SearchIcon className='w-[60%]  h-[50%] m-0 p-0 mt-[2%] text-black ' />
+                    <Link href="/pages/searchcourse" onClick={() => handleSetActive('/pages/searchcourse')}>
+                        <div className={`nav-item ${active === '/pages/searchcourse' ? 'active' : ''}`}>
+                            <SearchIcon className="nav-icon" />
                             <p>Search Course</p>
                         </div>
                     </Link>
-                    <Link href="/pages/login" className='w-[25%] h-[100%]  flex justify-center items-center' >
-                        <div className='w-[100%] h-[100%] flex justify-center items-center   text-center text-black lg:text-2xl cursor-pointer flex-col  text-[0.60rem]' >
-                            <LoginIcon className='w-[60%]  h-[50%] m-0 p-0 mt-[2%] text-black ' />
+                    <Link href="/pages/login" onClick={() => handleSetActive('/pages/login')}>
+                        <div className={`nav-item ${active === '/pages/login' ? 'active' : ''}`}>
+                            <LoginIcon className="nav-icon" />
                             <p>Login</p>
                         </div>
                     </Link>
-                    <Link href="/pages/portal" className='w-[25%] h-[100%]  flex justify-center items-center' >
-                        <div className='w-[100%] h-[100%] flex justify-center items-center   text-center text-black lg:text-2xl cursor-pointer flex-col  text-[0.60rem]' >
-                            <PersonIcon className='w-[60%]  h-[50%] m-0 p-0 mt-[2%] text-black '/> 
+                    <Link href="/pages/portal" onClick={() => handleSetActive('/pages/portal')}>
+                        <div className={`nav-item ${active === '/pages/portal' ? 'active' : ''}`}>
+                            <PersonIcon className="nav-icon" />
                             <p>Portal</p>
                         </div>
                     </Link>
+
 
                 </div>
                 {props.children}
